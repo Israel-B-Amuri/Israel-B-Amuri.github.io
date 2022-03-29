93c9215a2977d7747e93f46fe55f9de9a1774040
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2022-03-29 12:08:27 +0300

    projects addition

---
+++ projects/Imaginary store/survey page/script.js
@@ -0,0 +1,6 @@
+let checkButton= document.querySelector('button')
+let inputBox= document.querySelector('input')
+checkButton.addEventListener('click'), ()=>{
+    let inputBox = document.querySelector('input')
+    console.log(inputBox.value)
+}
